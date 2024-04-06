82505094c12344c3c3e1bf851460f8c864633933
auxload <gd69435@gmail.com>
2024-04-06 14:00:06 +0300

    sex2

--- components/NavBar/Navigation.tsx
+++ components/NavBar/Navigation.tsx
@@ -14,18 +14,26 @@ import { cva } from "class-variance-authority";
 const navigationMenuTriggerStyle = cva(
   "group inline-flex h-10 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:underline hover:text-accent-foreground  focus:text-accent-foreground  disabled:pointer-events-none disabled:opacity-50 "
 );
-const Navigation = ({className,vertical}:{className?:string,vertical?:string}) => {
+const Navigation = ({
+  className,
+  vertical,
+}: {
+  className?: string;
+  vertical?: string;
+}) => {
   const urlPath = usePathname();
   return (
-    <NavigationMenu className={cn("hidden md:flex",className)}>
-      <NavigationMenuList className={cn("text-foreground/50",vertical)}>
+    <NavigationMenu className={cn("hidden md:flex", className)}>
+      <NavigationMenuList className={cn("text-foreground/50", vertical)}>
         {navConfig.map((navItem) => (
-          <NavigationMenuItem>
+          <NavigationMenuItem key={navItem.label}>
             <Link href={navItem.path} legacyBehavior passHref>
               <NavigationMenuLink
                 className={cn(
                   navigationMenuTriggerStyle(),
-                  `${urlPath === navItem.path && "text-foreground underline"} ${vertical && "text-lg my-2"}`
+                  `${urlPath === navItem.path && "text-foreground underline"} ${
+                    vertical && "text-lg my-2"
+                  }`
                 )}
               >
                 {navItem.label}
